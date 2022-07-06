b8c6c856c2e0e4a3d5a81c17599c6855709d25d6
jonasty <jonas.berx@ut.ee>
2022-07-06 17:06:41 +0300

    Correct test to pass

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders navbar title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const navbarTitle = screen.getByText(/Waiting Time Analyser/i);
+  expect(navbarTitle).toBeInTheDocument();
 });
